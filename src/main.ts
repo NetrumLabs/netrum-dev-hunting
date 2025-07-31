697fad1589f61db4d8002e80d53022eefcbf64fe
a3259454 <451580067@qq.com>
2025-07-31 09:05:34 +0200

    Added my developer profile

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ export const devRegistry = [
     name: "Saandy",
     folder: "Saandy-dev",
   },
-  
+  {
+    name: "cg1237910",
+    folder: "cg1237910-dev",
+  }  
   // 🔰 New devs just add entry here
 ];
