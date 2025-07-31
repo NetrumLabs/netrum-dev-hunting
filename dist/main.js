b7419fdc020ef5b60b414f0ba7d22e84b2620e89
John <63875204+cuongdt1994@users.noreply.github.com>
2025-07-31 21:35:41 +0700

    Added my developer profile
    Added my developer profile

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,8 @@
 // src/main.ts
 export const devRegistry = [
     {
-        name: "cuongdt1994",
-        folder: "cuongdt1994-dev",
+        name: "Saandy",
+        folder: "Saandy-dev",
     },
     // 🔰 New devs just add entry here
 ];
