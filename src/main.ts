69934032c8c0604fc30c4119ff0dd600cad0341c
Madhusahitya <madhusahitya2464@gmail.com>
2025-08-20 00:40:16 +0530

    Added my developer profile

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 export const devRegistry = [
-  {
-    name: "jaydaz",
-    folder: "jaydaz-dev"
-  }
+  { name: "jaydaz", folder: "jaydaz-dev" },
+  { name: "madhusahitya", folder: "madhusahitya" }
 ];
